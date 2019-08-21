a05abac3dc03b7104e11f60dfc94de707e151045
Marquise Stein <botocratic@gmail.com>
2019-08-20 21:15:50 -0700

    Import install-ipfs.sh

---
+++ install-ipfs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+################################################################################
+#
+#  Copyright (C) 2019 juztamau5
+#  This file is part of 8 Bit Bot - https://github.com/juztamau5/8-bit-bot
+#
+#  SPDX-License-Identifier: MIT
+#  See the file LICENSING for more information.
+#
+################################################################################
+
+sudo snap install ipfs
