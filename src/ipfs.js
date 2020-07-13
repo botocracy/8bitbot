f5ebcd52df2c477f3150ca681e02f34c043edaea
Marquise Stein <botocratic@gmail.com>
2020-07-13 14:49:29 -0700

    Add copyright headers

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2019-2020 Marquise Stein
+ * This file is part of 8bitbot - https://github.com/botocracy/8bitbot
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
 // IPFS parameters
 const IPFS_GATEWAYS = [
   'https://ipfs.io',
