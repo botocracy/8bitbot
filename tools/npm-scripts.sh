e177cd8c5a2c683a7ca84c4b46f9f014d7f77e0a
Marquise Stein <botocratic@gmail.com>
2020-07-25 11:31:39 -0700

    Refactor npm-scripts.sh

--- tools/npm-scripts.sh
+++ tools/npm-scripts.sh
@@ -9,16 +9,26 @@ function start() {
   snowpack dev
 }
 
+#
+# Helper function
+#
+# Usage:
+#
+#   patch_package <package name> <patch name>
+#
+function patch_package() {
+  package=$1
+  patch=$2
+
+  patch -p1 --forward --directory="node_modules/${package}" < \
+    "tools/depends/${package}/${patch}" || [[ "$?" == "1" ]]
+}
+
 function postinstall() {
   # Patch jsonld.js
-  for patch in \
-      "0001-Add-missing-webpack.config.js.patch" \
-      "0002-Switch-to-core-js-3.patch" \
-      "0003-Fix-exception-with-empty-process.version.patch" \
-  ; do
-    patch -p1 --forward --directory="node_modules/jsonld" < \
-      "tools/depends/jsonld.js/${patch}" || [ "$?" == "1" ]
-  done
+  patch_package "jsonld" "0001-Add-missing-webpack.config.js.patch"
+  patch_package "jsonld" "0002-Switch-to-core-js-3.patch"
+  patch_package "jsonld" "0003-Fix-exception-with-empty-process.version.patch"
 }
 
 function build() {
