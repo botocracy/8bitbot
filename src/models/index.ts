7a08213eb47330f5a31f4178eff0ac2e0e0f3af7
Marquise Stein <botocratic@gmail.com>
2020-10-13 05:47:38 -0700

    Revert "VideoPlayer"
    This reverts commit cd5a82d87dbdfaf231ab734fc6f96b2e99d19532.

--- src/models/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2019-2020 Marquise Stein
- * This file is part of 8bitbot - https://github.com/botocracy/8bitbot
- *
- * SPDX-License-Identifier: Apache-2.0
- * See LICENSE.txt for more information.
- */
-
-export * from './videos';
