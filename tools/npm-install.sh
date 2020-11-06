1bcb96c6dc6d772af817483a3fdc552998f4b94e
Marquise Stein <botocratic@gmail.com>
2020-11-06 13:13:15 -0800

    Refactor npm scripts

---
+++ tools/npm-install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+################################################################################
+#
+#  Copyright (C) 2019-2020 Marquise Stein
+#  This file is part of 8bitbot - https://github.com/botocracy/8bitbot
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See the file LICENSES/README.md for more information.
+#
+################################################################################
+
+#
+# NPM script entry point for installation-related tasks
+#
+# This file has been separated from npm-scripts.sh to aid in change-detection
+# for CI infrastructure.
+#
+# Call via:
+#
+#   npm-install.sh <task>
+#
+# See the function dispatch() for the available tasks that can be run.
+#
+
+# Enable strict mode
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Get the absolute path to this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Import utility functions
+source "${SCRIPT_DIR}/npm-utils.sh"
+
+#
+# Dispatch function
+#
+# This function contains the available tasks. The first argument identifies
+# which task to jump to.
+#
+function dispatch() {
+  case $1 in
+  postinstall)
+    postinstall
+    ;;
+  *)
+    echo "Invalid task: $1"
+    exit 1
+    ;;
+  esac
+}
+
+function postinstall() {
+  # Patch jsonld.js
+  patch_package "jsonld" "0001-Add-missing-webpack.config.js.patch"
+  patch_package "jsonld" "0002-Switch-to-core-js-3.patch"
+  patch_package "jsonld" "0003-Fix-exception-with-empty-process.version.patch"
+}
+
+# Perform the dispatch
+dispatch $1
