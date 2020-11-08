792bbc8b278096de46040643954a4072f2fe5851
Marquise Stein <botocratic@gmail.com>
2020-11-08 09:42:31 -0800

    VideoPlayer dependencies, models and player

---
+++ src/models/videos/playlist/video-playlist-type.model.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 Marquise Stein
+ * This file is part of 8bitbot - https://github.com/botocracy/8bitbot
+ *
+ * This file is derived from the PeerTube project, available under
+ * the AGPL 3.0 (or later) license. https://github.com/Chocobozzz/PeerTube
+ *
+ * SPDX-License-Identifier: Apache-2.0 AND AGPL-3.0-or-later
+ * See LICENSE.txt for more information.
+ */
+
+export const enum VideoPlaylistType {
+  REGULAR = 1,
+  WATCH_LATER = 2,
+}
