bfbc665227ccb53d3b83613db0d388f04f9eb8ed
Marquise Stein <botocratic@gmail.com>
2020-11-11 06:04:12 -0800

    Add Hardhat files

---
+++ hardhat.config.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2020 Marquise Stein
+ * This file is part of 8bitbot - https://github.com/botocracy/8bitbot
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+const dotenv = require('dotenv-defaults');
+
+// Inject Hardhat plugins
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-waffle');
+
+// See file ".env.defaults"
+dotenv.config();
+
+// TODO: reenable solidity-coverage when it works
+// require("solidity-coverage");
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+// To learn how to create Hardhat tasks go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const config /*: HardhatUserConfig*/ = {
+  defaultNetwork: 'hardhat',
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          evmVersion: 'berlin',
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+            details: {
+              yul: true,
+              deduplicate: true,
+              cse: true,
+              constantOptimizer: true,
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {},
+    localhost: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+    coverage: {
+      url: 'http://127.0.0.1:8555', // Coverage launches its own ganache-cli client
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+};
+
+module.exports = config;
