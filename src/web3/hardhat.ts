b04d4e524b1979f82276ab2c52b28c601a67430a
Marquise Stein <botocratic@gmail.com>
2020-11-28 11:42:02 -0800

    fixup! Add Hardhat and web3 files

---
+++ src/web3/hardhat.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020 Marquise Stein
+ * This file is part of 8bitbot - https://github.com/botocracy/8bitbot
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+import * as hardhat from 'hardhat';
+
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+
+export { hardhat };
